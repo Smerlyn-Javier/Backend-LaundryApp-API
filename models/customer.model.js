32daf1d0607ef78142c69b6c1b50c848a5dad7e7
Smerlyn Javier <smerlyn88@gmail.com>
2019-08-10 22:19:35 +0200

    Adding plan to customerSchema

--- models/customer.model.js
+++ models/customer.model.js
@@ -1,14 +1,16 @@
 const mongoose = require("mongoose");
 const {Schema} = mongoose;
-const {StatusHelper} = require("../helpers")
+const {StatusHelper,PlanHelper} = require("../helpers")
 
-const CustomerModel = new Schema({ name: { type: String },
+const CustomerModel = new Schema({
+    name: { type: String },
     lastname: { type: String },
     email: { type: String, unique: true, lowercase: true, required: true },
     username: { type: String, required: true, unique: true },
     password: { type: String, required: true },
     address: { type: String },
     picture: { type: String },
+    plan:{type:String, default:PlanHelper.NONE},
     status:{type:String , default:StatusHelper.ACTIVE}
     },
     {timestamps:{createdAt:true,updatedAt:true}})
