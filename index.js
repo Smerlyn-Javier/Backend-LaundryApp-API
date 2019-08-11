be2b81f7ab9da6dc6107ff3c89544babc8738d6b
Smerlyn Javier <smerlyn88@gmail.com>
2019-08-11 19:09:51 +0200

    Adding middleware express-json

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const app = express();
 const mongoose = require("mongoose");
 const {PORT,MONGO_URI} = require("./config");
 
+app.use(express.json());
+
 mongoose.connect(MONGO_URI,{ useNewUrlParser: true }).then(()=>{
     app.listen(PORT,()=>{
         return console.log(`Application running on por ${PORT}`)
